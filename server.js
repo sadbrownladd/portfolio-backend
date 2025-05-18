b61289753fecd66f576bff73dd9b20cc4656e0b9
Abdullah Asim <bscs23064@itu.edu.pk>
2025-05-18 22:52:03 +0500

    Add server.js with Express setup and routes

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const educationRoutes = require('./routes/educationRoutes');
+const skillRoutes = require('./routes/skillRoutes');
+const projectRoutes = require('./routes/projectRoutes');
+const experienceRoutes = require('./routes/experienceRoutes');
+
+const app = express();
+
+// Middleware
+app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(express.json());
+
+// Routes
+app.use('/api/education', educationRoutes);
+app.use('/api/skills', skillRoutes);
+app.use('/api/projects', projectRoutes);
+app.use('/api/experiences', experienceRoutes);
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
