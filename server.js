aa30a9566a5324cc749f966c1d2057c723a1a6cc
Abdullah Asim <bscs23064@itu.edu.pk>
2025-05-18 23:43:49 +0500

    Add global error handling middleware to server.js

--- server.js
+++ server.js
@@ -23,6 +23,12 @@ app.use('/api/experiences', experienceRoutes);
 const connectDB = require('./config/db');
 connectDB();
 
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
+
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
