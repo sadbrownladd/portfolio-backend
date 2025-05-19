6ceeb8834afdf9f11938f4c1e77f15a66e0092ce
Abdullah Asim <bscs23064@itu.edu.pk>
2025-05-19 20:16:04 +0500

    Add Contact route to server

--- server.js
+++ server.js
@@ -1,34 +1,30 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
+const mongoose = require('mongoose');
 const educationRoutes = require('./routes/educationRoutes');
 const skillRoutes = require('./routes/skillRoutes');
 const projectRoutes = require('./routes/projectRoutes');
 const experienceRoutes = require('./routes/experienceRoutes');
+const contactRoutes = require('./routes/contactRoutes');
 
 const app = express();
 
-// Middleware
+require('dotenv').config();
+
 app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 
-// Routes
 app.use('/api/education', educationRoutes);
 app.use('/api/skills', skillRoutes);
 app.use('/api/projects', projectRoutes);
-app.use('/api/experiences', experienceRoutes);
+app.use('/api/experience', experienceRoutes);
+app.use('/api/contact', contactRoutes);
 
-// Database Connection
+const PORT = process.env.PORT || 5000;
+
 const connectDB = require('./config/db');
 connectDB();
 
-// Error Handling Middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: 'Something went wrong!' });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-// Start Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
